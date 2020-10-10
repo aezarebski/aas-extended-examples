52044b04768ba935d327b4eaa337a844d30fd413
aezarebski <aezarebski@gmail.com>
2020-10-10 14:59:31 +0100

    include a simple visualisation and estimation of the parameters

--- demo.py
+++ demo.py
@@ -1,9 +1,18 @@
 import pandas as pd
 from scipy import stats
-import numpy as np
-from typing import List, Any
+import altair as alt
+from typing import List, Any, Tuple
 from functools import reduce
+import math as math
 
+EstimateAndCI = Tuple[float,Tuple[float,float]]
+
+
+def wald_estimate_and_ci(num_trials: int, num_success: int) -> EstimateAndCI:
+    p_hat = num_success / num_trials
+    z = 1.96
+    delta = z * math.sqrt(p_hat * (1 - p_hat) / num_trials)
+    return (p_hat,(p_hat - delta, p_hat + delta))
 
 def random_flips(num_flips: int,
                  prob_heads: float,
@@ -27,17 +36,42 @@ def random_experiment(num_flips: int,
 
 
 def main():
-    num_flips = 10
+    num_flips = 30
     num_people = 15
     person_ids = range(num_people)
     num_outliers = 2
-    probs_heads_list = ((num_people - num_outliers) * [0.5] +
-                        num_outliers * [0.8])
+    probs_heads_list = ((num_people - num_outliers) * [0.4] +
+                        num_outliers * [0.95])
     experiment_results = random_experiment(num_flips,
                                            person_ids,
                                            probs_heads_list)
-    return experiment_results
 
+    bar = experiment_results.drop(columns = "flip_number").groupby("name").sum()
+    bar["name"] = bar.index.copy()
+    return bar
+
+
+bar = main()
+chart = alt.Chart(bar).mark_bar().encode(
+    alt.X("outcome:Q", bin = True),
+    y = 'count()'
+)
+
+# To actually display the chart use chart.show() to start a local server to
+# display it.
+
+bar2 = bar.loc[bar['outcome'] < 22]
+
+
+
+# >>> bar['outcome'].sum()
+# 206
+# >>> wald_estimate_and_ci(30 * 15, 206)
+# (0.4577777777777778, (0.41174514399055895, 0.5038104115649966))
+# >>> bar2['outcome'].sum()
+# 148
+# >>> wald_estimate_and_ci(30 * 13, 148)
+# (0.37948717948717947, (0.33132593489635276, 0.4276484240780062))
 
 if __name__ == '__main__':
     main()
