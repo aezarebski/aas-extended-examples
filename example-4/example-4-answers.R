444df31d281993f7b4fc3d6a33e9659ba66c14ae
Alex Zarebski <aezarebski@gmail.com>
2020-12-02 13:31:56 +0000

    sketch a basic analysis of the dataset

--- example-4/example-4-answers.R
+++ example-4/example-4-answers.R
@@ -3,39 +3,61 @@ library(purrr)
 library(magrittr)
 library(ggplot2)
 library(lme4)
+library(MuMIn)
 
-x <- read.csv("cat-weights-by-breed.csv", sep = " ")
+CAT_DF <- read.csv("cat-weights-by-breed.csv", sep = " ")
 
-.id_sample <- sample(unique(x$id), size = 12)
+.id_sample <- sample(unique(CAT_DF$id), size = 12)
 
 
-g1 <- ggplot(filter(x, is.element(id, .id_sample)), aes(x = age, y = weight, group = id)) + geom_point() + facet_wrap(~id)
+g1 <- ggplot(filter(CAT_DF, is.element(id, .id_sample)), aes(x = age, y = weight, group = id)) +
+  geom_point() +
+  facet_wrap(~id)
 
 
 summarise_fit_factory <- function(summary_func) {
   function(id_num) {
-    fit <- lm(weight ~ poly(age, 2),
-              data = x[x$id == id_num,])
+    fit <- lm(weight ~ poly(age, 2, raw = TRUE),
+      data = CAT_DF[CAT_DF$id == id_num, ]
+    )
     summary_func(fit)
   }
 }
 demo_coefs <- summarise_fit_factory(coef)
 demo_fit <- summarise_fit_factory((function(x) x$fit))
 
+individual_model_coefs <- map(
+  .x = unique(CAT_DF$id),
+  .f = demo_coefs
+)
 
-individual_model_fits <- map(.x = unique(x$id),
-           .f = function(x) {data.frame(age = 1:20, weight_fit = demo_fit(x), id = x)}) %>% bind_rows
+individual_model_fits <- map(
+  .x = unique(CAT_DF$id),
+  .f = function(x) {
+    data.frame(age = 1:20, weight_fit = demo_fit(x), id = x)
+  }
+) %>% bind_rows()
 
 
 
-g2 <- ggplot() +
-  geom_point(
-  data = filter(x, is.element(id, .id_sample)),
-  mapping = aes(x = age, y = weight)) +
+g2 <- g1 +
   geom_line(
-  data = filter(individual_model_fits, is.element(id, .id_sample)),
-  mapping = aes(x = age, y = weight_fit, group = id)) + facet_wrap(~id)
+    data = filter(individual_model_fits, is.element(id, .id_sample)),
+    mapping = aes(x = age, y = weight_fit, group = id),
+    linetype = "dashed"
+  ) + facet_wrap(~id)
 
+quadratic_model <- lmer(weight ~ poly(age, 2, raw = TRUE) + (poly(age, 2, raw = TRUE) | id),
+  data = CAT_DF
+)
 
-## Do the algebra to work out the appropriate definition for a LMM for this data
-## set.
+confint.merMod(quadratic_model, "poly(age, 2, raw = TRUE)1", method = "Wald")
+
+r.squaredGLMM(quadratic_model)
+
+cat_df_with_lme_fit <- CAT_DF
+cat_df_with_lme_fit$fit <- fitted(quadratic_model)
+
+g3 <- g2 + geom_line(
+             data = filter(cat_df_with_lme_fit, is.element(id, .id_sample)),
+           mapping = aes(x = age, y = fit, group = id), linetype = "solid")
