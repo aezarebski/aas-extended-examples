00c83354da4e8fd79189e4b14a3d98480dae9ae2
Alex Zarebski <aezarebski@gmail.com>
2021-11-06 17:46:49 +0000

    demonstrate the use of formulas

--- example-2.5/scratch.R
+++ example-2.5/scratch.R
@@ -31,8 +31,15 @@ summary(m2)
 m3 <- lm(mpg ~ . - name - weight - acceleration, data = df2)
 summary(m3)
 
+#' Observe that you can also do this with the original data set by modifying the formula.
+m3b <- lm(mpg ~ . - name - weight + log(weight) - acceleration + I(acceleration^2), data = df1)
+summary(m3b)
+
 ## Observe that if you consider the interaction between year and the country
 ## that you find that the MPG is improving faster in Europe and Japan than in
 ## America.
-m4 <- lm(mpg ~ . + origin*year - name - weight - acceleration - horsepower, data = df2)
-summary(m4)
+m4a <- lm(mpg ~ . - name - weight + log(weight) - acceleration + I(acceleration^2), data = df1)
+summary(m4a)
+
+m4b <- lm(mpg ~ . + origin*year - name - weight + log(weight) - acceleration + I(acceleration^2), data = df1)
+summary(m4b)
