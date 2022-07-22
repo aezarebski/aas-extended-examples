7289529ed51a1cfde72bbc831d3bf9978d8a9a30
Alexander E. Zarebski <36244258+aezarebski@users.noreply.github.com>
2022-07-22 12:53:45 +0100

    Security fix (#44)
    * remove requirements file because this is mainly jupyter notebooks
    
    * docs
    
    * get nix-shell working again

--- shell.nix
+++ shell.nix
@@ -1,40 +1,27 @@
-
 # This provides a shell running a jupyter lab server. To start the server run
 # the following command
 #
 # $ nix-shell --command "jupyter lab"
+#
 
 let
   jupyter = import (builtins.fetchGit {
     url = https://github.com/tweag/jupyterWith;
-    rev = "";
+    rev = "79dd05b3d703cd0034296dd367422f7298fe99d6";
   }) {};
 
-  irkernel = jupyter.kernels.iRWith {
-    name = "r";
-    packages = p: with p; [ p.ggplot2
-                            p.nnet
-                            p.MASS
-                            p.lme4
-                            p.MuMIn
-                            p.dplyr
-                            p.purrr
-                            p.magrittr
-                            p.effects];
-  };
-
-  ipython = jupyter.kernels.iPythonWith {
+  iPython = jupyter.kernels.iPythonWith {
     name = "python";
-    packages = p: with p; [ numpy
-                            altair
-                            statsmodels
+    packages = p: with p; [ matplotlib
+                            numpy
                             pandas
-                            matplotlib ];
+                            scipy
+                            statsmodels ];
   };
 
-  jupyterEnvironment = jupyter.jupyterlabWith {
-    kernels = [ ipython
-                irkernel ];
-  };
+  jupyterEnvironment =
+    jupyter.jupyterlabWith {
+      kernels = [ iPython ];
+    };
 in
   jupyterEnvironment.env
